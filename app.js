5a8eab30eff1fa949b222bda9b42f23a721d19d1
Sahil.Sharma3184@gmail.com <sahil.sharma3184@gmail.com>
2025-07-11 16:46:58 +0530

    made changes in ejs files ui

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing=require("./models/listing.js");
 const path=require("path");
 const methodOverride = require('method-override');
 const ejsMate=require("ejs-mate"); //for using repetitive layouts
+const multer = require("multer");
+const upload = multer({ dest: 'uploads/' }); // stores files in uploads/ folder
+
 
 
 const MONGO_URL="mongodb://127.0.0.1:27017/Nivasa"
@@ -27,7 +30,9 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));//sara data request ke andr parse ho paye
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
+app.use(express.json());
 app.use(express.static(path.join(__dirname,"/public")));
+
 //index route
 app.get("/listings",async(req,res)=>{
     const allListings=await Listing.find({});
@@ -51,7 +56,7 @@ app.get("/listings/:id",async(req,res)=>{
 //create route
 
 // Handles POST request to /listings (form submission route)
-app.post("/listings", async (req, res) => {                     // Handle POST request to create new listing
+app.post("/listings", upload.single('listing[image]'), async (req, res) => {    // Handle POST request to create new listing
     const newListing = new Listing(req.body.listing);           // Create a new listing from form data
     await newListing.save();                                    // Save the new listing to the database
     res.redirect("/listings");                                  // Redirect to the listings page after saving
