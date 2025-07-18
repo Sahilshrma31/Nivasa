7dcf9cdec11dfbfba2a17356a73c6015d9d88aeb
Sahil.Sharma3184@gmail.com <sahil.sharma3184@gmail.com>
2025-07-18 20:33:43 +0530

    feat:added map feature on show listings

--- controllers/listing.js
+++ controllers/listing.js
@@ -15,9 +15,9 @@ module.exports.showListing = async (req, res) => {
     const { id } = req.params;
     const listing = await Listing.findById(id)
       .populate({
-        path:"reviews",
-        populate:{
-          path:"author",
+        path: "reviews",
+        populate: {
+          path: "author",
         },
       })
       .populate("owner");
@@ -26,33 +26,38 @@ module.exports.showListing = async (req, res) => {
       req.flash("error", "Listing you requested for does not exist");
       return res.redirect("/listings");
     }
-    // This makes sure .equals() won't throw an error in EJS
+  
     if (req.user) {
       req.user._id = new mongoose.Types.ObjectId(req.user._id);
     }
-    //  Pass currUser to EJS
-    res.render("listings/show.ejs", { listing, currUser: req.user });
+  
+    res.render("listings/show.ejs", {
+      listing,
+      currUser: req.user,
+      MAP_TOKEN: process.env.MAP_TOKEN, // ✅ Now accessible in EJS
+    });
   };
-
-  module.exports.createListing=async (req, res) => { //error handle karne ke liye async function wrapAsync se wrapped hai
-    let url=req.file.path;
-    let filename=req.file.filename;
-    const listingData = req.body.listing; //form se aaya hua listing data extract kar rahe hain
-
-    if (req.file) { //agar koi file upload hui hai
+  
+  module.exports.createListing = async (req, res) => {
+    const listingData = req.body.listing; // extract listing data from the form
+  
+    if (req.file) {
+      // only access path and filename if file was uploaded
       listingData.image = {
-        url: req.file.path, //image ka path save kar rahe hain
-        filename: req.file.filename, //image ka filename save kar rahe hain
+        url: req.file.path,
+        filename: req.file.filename,
       };
     }
-
-    const newListing = new Listing(listingData); //listingData ke basis pe naya Listing object banaya
-    newListing.owner=req.user._id; //current logged in user ko as owner assign kiya
-    await newListing.save(); //listing ko database me save kiya
-
-    req.flash("success", "New listing created successfully."); //success message flash kiya
-    res.redirect(`/listings`); //user ko listings page pe redirect kiya
+  
+    const newListing = new Listing(listingData);
+    newListing.owner = req.user._id;
+  
+    await newListing.save();
+  
+    req.flash("success", "New listing created successfully.");
+    res.redirect(`/listings`);
   };
+  
 
   module.exports.renderEditForm=async (req, res) => {
     const { id } = req.params;
